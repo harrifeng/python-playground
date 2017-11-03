6428686c668fa578105536cf045295fc4adf46b7
Haoran Feng <harrifeng@gmail.com>
2017-11-03 08:12:58 +0000

    not thread safe now

--- 002-thread-safe/main.py
+++ 002-thread-safe/main.py
@@ -1,15 +1,23 @@
 import thread
 
+gl  = 35
 
-def worker(s):
-    print 'hello ' + str(s)
+def worker(num):
+    global gl
+    num = int(num)
+    ourstr = ''
+    for i in range(num):
+        ourstr += str(i)
+        gl += 1
+        print(gl)
+    # print(ourstr)
     return
 
 
 if __name__ == '__main__':
-    N = 5
+    N = 50
     while True:
         for i in range(N):
-            thread.start_new_thread(worker, ('world',))
+            thread.start_new_thread(worker, (15,))
         if input() == 'q':
             break
